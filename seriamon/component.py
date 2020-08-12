f32bd5a4084695667c72f0de0dde60eeb97756b3
Zou Hanya <hanyazou@gmail.com>
2020-08-12 20:21:56 +0900

    Add SeriaMonComponent super class

---
+++ seriamon/component.py
@@ -0,0 +1,74 @@
+from PyQt5.QtWidgets import *
+
+class SeriaMonComponent:
+
+    def __init__(self, compId, sink, instanceId=0):
+        self.compId = compId
+        self.sink = sink
+
+    def savePreferences(self, prefs):
+        for prop in self.preferencePoperties:
+            typ = prop[0]
+            name = prop[1]
+            value = getattr(self, name)
+            if typ == bool:
+                value = int(value)
+            prefs[self.preferenceKeyPrefix + name] = str(value)
+
+    def loadPreferences(self, prefs):
+        for prop in self.preferencePoperties:
+            typ = prop[0]
+            name = prop[1]
+            key = self.preferenceKeyPrefix + name
+            if not key in prefs:
+                continue
+            value = str(prefs[key])
+            if not value:
+                continue
+            if typ == bool:
+                value = int(value)
+            setattr(self, name, typ(value))
+        self.reflectToUi()
+
+    def initPreferences(self, prefix, prefprops):
+        self.preferenceKeyPrefix = prefix
+        self.preferencePoperties = prefprops
+        for prop in prefprops:
+            typ = prop[0]
+            name = prop[1]
+            value = prop[2]
+            setattr(self, name, typ(value))
+
+    def reflectToUi(self):
+        for prop in self.preferencePoperties:
+            typ = prop[0]
+            name = prop[1]
+            if 4 <= len(prop):
+                widget = prop[3]
+            else:
+                widget = None
+            if typ in (str, int, float) and isinstance(widget, QComboBox):
+                index = widget.findData(typ(getattr(self, name)))
+                if 0 <= index:
+                    widget.setCurrentIndex(index)
+                else:
+                    print('WARNING: failed to reflect {} to UI'.format(name))
+            elif typ is bool and isinstance(widget, QCheckBox):
+                widget.setChecked(typ(getattr(self, name)))
+            elif not widget is None:
+                print('WARNING: failed to reflect {} to UI'.format(name))
+
+    def reflectFromUi(self):
+        for prop in self.preferencePoperties:
+            typ = prop[0]
+            name = prop[1]
+            if 4 <= len(prop):
+                widget = prop[3]
+            else:
+                widget = None
+            if isinstance(widget, QComboBox):
+                setattr(self, name, typ(widget.currentData()))
+            elif typ is bool and isinstance(widget, QCheckBox):
+                setattr(self, name, typ(widget.isChecked()))
+            elif not widget is None:
+                print('WARNING: failed to reflect {} from UI'.format(name))
--- seriamon/uart.py
+++ seriamon/uart.py
@@ -7,14 +7,13 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 from PyQt5.QtCore import QVariant
 from PyQt5.QtGui import QTextCursor
+from .component import SeriaMonComponent
 
-class UartReader(QWidget):
+class UartReader(QWidget, SeriaMonComponent):
 
     def __init__(self, compId, sink, instanceId=0):
-        super().__init__()
+        super().__init__(compId=compId, sink=sink, instanceId=instanceId)
 
-        self.compId = compId
-        self.sink = sink
         self.thread = _ReaderThread(self)
         self.port = serial.Serial()
 
@@ -78,73 +77,6 @@ class UartReader(QWidget):
         self.setLayout(grid)
 
         self.thread.start()
-
-    def savePreferences(self, prefs):
-        for prop in self.preferencePoperties:
-            typ = prop[0]
-            name = prop[1]
-            value = getattr(self, name)
-            if typ == bool:
-                value = int(value)
-            prefs[self.preferenceKeyPrefix + name] = str(value)
-
-    def loadPreferences(self, prefs):
-        for prop in self.preferencePoperties:
-            typ = prop[0]
-            name = prop[1]
-            key = self.preferenceKeyPrefix + name
-            if not key in prefs:
-                continue
-            value = str(prefs[key])
-            if not value:
-                continue
-            if typ == bool:
-                value = int(value)
-            setattr(self, name, typ(value))
-        self.reflectToUi()
-
-    def initPreferences(self, prefix, prefprops):
-        self.preferenceKeyPrefix = prefix
-        self.preferencePoperties = prefprops
-        for prop in prefprops:
-            typ = prop[0]
-            name = prop[1]
-            value = prop[2]
-            setattr(self, name, typ(value))
-
-    def reflectToUi(self):
-        for prop in self.preferencePoperties:
-            typ = prop[0]
-            name = prop[1]
-            if 4 <= len(prop):
-                widget = prop[3]
-            else:
-                widget = None
-            if typ in (str, int, float) and isinstance(widget, QComboBox):
-                index = widget.findData(typ(getattr(self, name)))
-                if 0 <= index:
-                    widget.setCurrentIndex(index)
-                else:
-                    print('WARNING: failed to reflect {} to UI'.format(name))
-            elif typ is bool and isinstance(widget, QCheckBox):
-                widget.setChecked(typ(getattr(self, name)))
-            elif not widget is None:
-                print('WARNING: failed to reflect {} to UI'.format(name))
-
-    def reflectFromUi(self):
-        for prop in self.preferencePoperties:
-            typ = prop[0]
-            name = prop[1]
-            if 4 <= len(prop):
-                widget = prop[3]
-            else:
-                widget = None
-            if isinstance(widget, QComboBox):
-                setattr(self, name, typ(widget.currentData()))
-            elif typ is bool and isinstance(widget, QCheckBox):
-                setattr(self, name, typ(widget.isChecked()))
-            elif not widget is None:
-                print('WARNING: failed to reflect {} from UI'.format(name))
 
     def _buttonClicked(self):
         sender = self.sender()
