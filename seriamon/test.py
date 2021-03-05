495f41a5abb09a68c9adc6824af068aac1e076b2
Zou Hanya <hanyazou@gmail.com>
2021-03-05 15:50:21 +0900

    Add timeout handling to show data transfer loss

--- seriamon/test.py
+++ seriamon/test.py
@@ -17,6 +17,7 @@ class UartTester(UartReader):
         port.timeout = 5.0 # seconds
 
         self.testInputCount = 0
+        self.testInputTotal = 0
         self.testInputCRC = 0
         self.testInputNoCRC = True
         self.testInputStart = 0
@@ -24,6 +25,7 @@ class UartTester(UartReader):
         self.inputTotal = 0
 
         self.testOutputCount = 0
+        self.testOutputTotal = 0
         self.testOutputCRC = 0
         self.testOutputNoCRC = True
         self.testOutputStart = 0
@@ -31,6 +33,7 @@ class UartTester(UartReader):
         self.outputTotal = 0
 
     def _portHandler(self, port, types):
+        command = None
         if 0 < self.testInputCount or 0 < self.testOutputCount:
             """
                send or receive test data stream
@@ -41,17 +44,21 @@ class UartTester(UartReader):
                 """
                 buf = port.read(min(self.testInputCount, self.BUFFER_SIZE))
                 n = len(buf)
-                if not self.testInputNoCRC:
-                    self.testInputCRC = zlib.crc32(buf, self.testInputCRC) & 0xffffffff
-                self.testInputCount -= n
-                self.inputTotal += n
-                self.sink.putLog("{} bytes recieved, {} bytes remain".format(
-                    n, self.testInputCount))
-                if self.testInputCount == 0:
+                if n == 4 and buf.decode() == "\\TGS":
+                    command = "\\TGS"
+                    self.testInputCount = 0
+                else:
                     self.testInputEnd = datetime.now().timestamp() * 1000
+                    if not self.testInputNoCRC:
+                        self.testInputCRC = zlib.crc32(buf, self.testInputCRC) & 0xffffffff
+                    self.testInputCount -= n
+                    self.inputTotal += n
+                    self.sink.putLog("{:,} bytes recieved, {:,} bytes {:.1f}% remain".format(
+                        n, self.testInputCount, self.testInputCount * 100 / self.testInputTotal))
+                if self.testInputCount == 0:
                     crc = self._readUint32(port)
                     self.sink.putLog(
-                        "{} bytes recieved in {:7.3f} sec, CRC: {:08x}{}{:08x}".format(
+                        "{:,} bytes recieved in {:7.3f} sec, CRC: {:08x}{}{:08x}".format(
                             self.inputTotal,
                             (self.testInputEnd - self.testInputStart) / 1000.0,
                             crc,
@@ -71,12 +78,13 @@ class UartTester(UartReader):
                     self.testOutputCRC = zlib.crc32(buf, self.testOutputCRC) & 0xffffffff
                 self.testOutputCount -= n
                 self.outputTotal += n
-                self.sink.putLog("{} bytes sent, {} bytes remain".format(n, self.testInputCount))
+                self.sink.putLog("{:,} bytes sent, {:,} bytes {:.1f}% remain".format(
+                    n, self.testOutputCount, self.testOutputCount * 100 / self.testOutputTotal))
                 if self.testOutputCount == 0:
                     self.testOutputEnd = datetime.now().timestamp() * 1000
                     self._writeUint32(port, self.testOutputCRC)
                     self.sink.putLog(
-                        "{} bytes sent in {:7.3f} sec, CRC: {:08x}".format(
+                        "{:,} bytes sent in {:7.3f} sec, CRC: {:08x}".format(
                             self.outputTotal,
                             (self.testOutputEnd - self.testOutputStart) / 1000.0,
                             self.testOutputCRC))
@@ -89,90 +97,97 @@ class UartTester(UartReader):
                 return
 
             buf = self._read(port, 4)
+            if len(buf) == 0:
+                return # timeout
+
             command = buf.decode()
 
-            if len(buf) == 0:
-                pass # timeout
+        if command == None:
+            pass
 
-            elif command == "\\TRC" or command == "\\TR_":
-                """
-                   comand: test receive with or w/o crc
-                """
-                if buf[3] == ord('C'):
-                    self.testInputNoCRC = False
-                else:
-                    self.testInputNoCRC = True
-                self.testInputCRC = 0
-                self.testInputCount = self._readUint32(port)
-                self.testInputStart = datetime.now().timestamp() * 1000
-                self.testInputEnd = 0
-                self.sink.putLog("testInputCount={} CRC={:08x}".format(
-                    self.testInputCount, self.testInputCRC))
-
-            elif command == "\\TSC" or command == "\\TS_":
-                """
-                   command: test send with or w/o crc
-                """
-                if buf[3] == ord('C'):
-                    self.testOutputNoCRC = False
-                else:
-                    self.testOutputNoCRC = True
-
-                self.testOutputCRC = 0
-                self.testOutputCount = self._readUint32(port)
-                self.testOutputStart = datetime.now().timestamp() * 1000
-                self.testOutputEnd = 0
-                self.sink.putLog("testOutputCount={} CRC={:08x}".format(
-                    self.testOutputCount, self.testOutputCRC))
-
-            elif command == "\\TGS":
-                """
-                   command: command: test get status
-                """
-                self.sink.putLog(
-                    "Test Get Status: {} {:08x} {} {:08x}".format(
-                        self.inputTotal, self.testInputCRC,
-                        self.outputTotal, self.testOutputCRC))
-                self._writeUint32(port, self.inputTotal)
-                self._writeUint32(port, self.testInputCRC)
-                self._writeUint32(port, self.outputTotal)
-                self._writeUint32(port, self.testOutputCRC)
-                self._write(port, self.send_data[:240])
-
-            elif command == "\\TRS":
-                """
-                   command: test reset status
-                """
-                self.sink.putLog("Test Reset Status")
-                self.testInputCount = 0
-                self.testInputCRC = 0
-                self.inputTotal = 0
-                self.testOutputCount = 0
-                self.testOutputCRC = 0
-                self.outputTotal = 0
-            elif command == "\\MSG":
-                """
-                   command: message from accessory
-                """
-                n = self._readUint32(port)
-                self.sink.putLog("Message {} bytes from accessory".format(n))
-                buf = self._read(port, n)
-                self.sink.putLog("    {}".format(self._bytesToHexString(buf)))
-            elif command == "\\ECH":
-                """
-                   command: echo 
-                """
-                n = self._readUint32(port)
-                self.sink.putLog("Echo {} bytes to accessory".format(n))
-                buf = self._read(port, n)
-                self.sink.putLog("    {}".format(self._bytesToHexString(buf)))
-                self._write(port, buf)
+        elif command == "\\TRC" or command == "\\TR_":
+            """
+               comand: test receive with or w/o crc
+            """
+            if buf[3] == ord('C'):
+                self.testInputNoCRC = False
             else:
-                self.sink.putLog("Unknown command from accessory")
-                if 0 < port.in_waiting:
-                    buf += self._read(port, port.in_waiting)
-                self.sink.putLog("    buffer: {}".format(self._bytesToHexString(buf)))
-                self.sink.putLog("    buffer: {}".format(buf.decode()))
+                self.testInputNoCRC = True
+            self.testInputCRC = 0
+            self.testInputCount = self._readUint32(port)
+            self.testInputTotal = self.testInputCount
+            self.testInputStart = datetime.now().timestamp() * 1000
+            self.testInputEnd = 0
+            self.sink.putLog("testInputCount={} CRC={:08x}".format(
+                self.testInputCount, self.testInputCRC))
+
+        elif command == "\\TSC" or command == "\\TS_":
+            """
+               command: test send with or w/o crc
+            """
+            if buf[3] == ord('C'):
+                self.testOutputNoCRC = False
+            else:
+                self.testOutputNoCRC = True
+
+            self.testOutputCRC = 0
+            self.testOutputCount = self._readUint32(port)
+            self.testOutputTotal = self.testOutputCount
+            self.testOutputStart = datetime.now().timestamp() * 1000
+            self.testOutputEnd = 0
+            self.sink.putLog("testOutputCount={} CRC={:08x}".format(
+                self.testOutputCount, self.testOutputCRC))
+
+        elif command == "\\TGS":
+            """
+               command: command: test get status
+            """
+            self.sink.putLog(
+                "Test Get Status: {} {:08x} {} {:08x}".format(
+                    self.inputTotal, self.testInputCRC,
+                    self.outputTotal, self.testOutputCRC))
+            self._writeUint32(port, self.inputTotal)
+            self._writeUint32(port, self.testInputCRC)
+            self._writeUint32(port, self.outputTotal)
+            self._writeUint32(port, self.testOutputCRC)
+            self._write(port, self.send_data[:240])
+
+        elif command == "\\TRS":
+            """
+               command: test reset status
+            """
+            self.sink.putLog("Test Reset Status")
+            self.testInputCount = 0
+            self.testInputTotal = 0
+            self.testInputCRC = 0
+            self.inputTotal = 0
+            self.testOutputCount = 0
+            self.testOutputTotal = 0
+            self.testOutputCRC = 0
+            self.outputTotal = 0
+        elif command == "\\MSG":
+            """
+               command: message from accessory
+            """
+            n = self._readUint32(port)
+            self.sink.putLog("Message {} bytes from accessory".format(n))
+            buf = self._read(port, n)
+            self.sink.putLog("    {}".format(self._bytesToHexString(buf)))
+        elif command == "\\ECH":
+            """
+               command: echo 
+            """
+            n = self._readUint32(port)
+            self.sink.putLog("Echo {} bytes to accessory".format(n))
+            buf = self._read(port, n)
+            self.sink.putLog("    {}".format(self._bytesToHexString(buf)))
+            self._write(port, buf)
+        else:
+            self.sink.putLog("Unknown command from accessory")
+            if 0 < port.in_waiting:
+                buf += self._read(port, port.in_waiting)
+            self.sink.putLog("    buffer: {}".format(self._bytesToHexString(buf)))
+            self.sink.putLog("    buffer: {}".format(buf.decode()))
 
     def _bytesToHexString(self, buf):
         return ''.join(["%02x " % ord(chr(x)) for x in buf]).strip()
